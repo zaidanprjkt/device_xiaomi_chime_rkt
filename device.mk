c14e3a2c35b457b64bedce39cee33357a9d1ea53
frost. (frostg-012) <frost@nusantararom.org>
2022-06-04 15:52:05 +0700

    chime: Provide android.hardware.power@1.3 on vendor
     * Needed by android.hardware.gnss@2.1-service-qti
    
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I74b7f14ab5bf240596c0b1c5aae3a5479013ccfd
    Signed-off-by: Raya Mahardika (rktdnt.) <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -432,6 +432,7 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.chime-libperfmgr \
+    android.hardware.power@1.3.vendor \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
