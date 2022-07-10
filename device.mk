989b033973b7d6e4691111d91fa280601bcb1421
frost. (frostg-012) <frost@nusantararom.org>
2022-07-10 17:22:55 +0700

    chime: Build CarrierConfigOverlay
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I627010e4185a9963076909495de82c94ea3b38b5
    Signed-off-by: rktdnt <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# CarrierConfig
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlay
+
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
