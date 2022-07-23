db9fb3ec1de0e7030e98868ef9a6d9b43a800282
frost. (frostg-012) <frost@nusantararom.org>
2022-07-23 15:53:13 +0700

    chime: Import Mi USB script from stock
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: If00a94466a28648056cecec0d14470a093d34aaf
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>
    Signed-off-by: csmqf <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -370,6 +370,7 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
+    init.mi.usb.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
