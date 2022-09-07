e23156ae06f313f9ff6e9938cf01cb6bbc0f9d40
strwbrry143 <alexfinhart@gmail.com>
2022-09-07 09:54:21 +0800

    chime: Adapt boardconfig kernel flags for AOSP Clang/LLVM binutils
    * adapt to lineage's recent kernel changes [1]
    * also manually enable llvm support to avoid vsdo errors since lineage only supports enabling llvm binutils by default for 5.10+ kernels (maybe not generic for 4.19)
    
    [1]https://github.com/LineageOS/android_vendor_lineage/commits/lineage-20.0/config/BoardConfigKernel.mk
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>
    Signed-off-by: rktdnt <rktdnt@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,8 +111,8 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/chime
 TARGET_KERNEL_CONFIG := vendor/chime_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
-TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1
+NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"  AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1
 
 # Media
 TARGET_USES_ION := true
