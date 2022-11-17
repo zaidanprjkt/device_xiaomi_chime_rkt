9f69ed0711023d1a1b9f600534dc8162a7d0355d
ryyaap. (unlstedd) <rktdnt@proton.me>
2022-11-17 23:34:30 +0700

    chime: Drop Google Battery HAL
    * Useless
    
    Revert "chime: Enable adaptive charging feature"
    
    This reverts commit c88524a02d45aad388bb78a65f7a114c65807f80.
    
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>
    
    Revert "chime: Add Google Battery HAL entry"
    
    This reverts commit 4eadab0e7040d7b2ff907bfd701ffea4ee35cd9c.
    
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>
    
    Revert "chime: Build Google Battery HAL"
    
    This reverts commit e3db8a77c2c7b6e381bae0e1c3decd9f870c721c.
    
    Signed-off-by: ryyaap. (unlstedd) <rktdnt@proton.me>

--- device.mk
+++ device.mk
@@ -37,10 +37,6 @@ OVERRIDE_TARGET_FLATTEN_APEX := true
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
-# Adaptive Charging
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/adaptivecharging.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/adaptivecharging.xml
-
 # Adaptive Suspend
 PRODUCT_PROPERTY_OVERRIDES += \
     suspend.short_suspend_threshold_millis=2000 \
@@ -248,10 +244,6 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
-
-# Google Battery HAL
-PRODUCT_PACKAGES += \
-    vendor.google.google_battery@1.1
 
 # GPS
 PRODUCT_PACKAGES += \
