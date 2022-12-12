kidzz1965/device_xiaomi_chime

chime: sepolicy: Initial specific device sepolicy
- Pull from Xiaomi SM8250

Co-authored-by: Pig <pig.priv@gmail.com>
Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
Co-authored-by: Utsav Balar <utsavbalar1231@gmail.com>
Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
Co-authored-by: boedhack99 <boedhack@gmail.com>
Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
Signed-off-by: HeroBuxx <herobuxx@gmail.com>

BoardConfig.mk
@@ -179,6 +179,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Security patch level

zaidanprjkt/device_xiaomi_chime_rkt

Co-authored-by: HeroBuxx <herobuxx@gmail.com>
Signed-off-by: csmqf <rktdnt@proton.me>

BoardConfig.mk
@@ -173,6 +173,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Security patch level