d712c70e1435672cc02e8f19efc3c03071bb583b
csmqf <rktdnt@proton.me>
2022-12-12 17:07:04 +0700

    Initialize Trees For Unified POCO M3 And Redmi 9T/9T NFC/9 Power
    * Chime is custom codename for POCO M3 (Citrus) and Redmi 9T (Lime) unified tree
    
    Co-authored-by: HeroBuxx <me@buxxed.tk>
    Signed-off-by: csmqf <rktdnt@proton.me>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_chime.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_chime-user \
+    nad_chime-userdebug \
+    nad_chime-eng
---
+++ nad_chime.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from chime device
+$(call inherit-product, device/xiaomi/chime/device.mk)
+
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := nad_chime
+PRODUCT_DEVICE := chime
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
